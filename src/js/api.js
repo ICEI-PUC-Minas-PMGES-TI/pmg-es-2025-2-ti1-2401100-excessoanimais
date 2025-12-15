3d1b936fb40744a15f9d274e807f5f12b9dc81ef
Seu Nome <daniel.pacheco@sga.pucminas.br>
2025-12-14 21:58:26 -0300

    ORGANIZANDO ESTRUTURA

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = (window.APP_CONFIG && window.APP_CONFIG.API_BASE_URL) || "http://localhost:3000";
 
 const api = {
     get: async (endpoint) => {
